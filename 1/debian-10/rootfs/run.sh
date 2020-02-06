703182579c25f40dd74c1189bbae41090378b314
Jozef Fekiac <jozef.fekiac@ibm.com>
2020-02-06 12:06:17 +0100

    printing debug where to connect

--- 1/debian-10/rootfs/run.sh
+++ 1/debian-10/rootfs/run.sh
@@ -13,6 +13,7 @@ counter=0;
 res=1000;
 while [[ $res != 0 && $counter -lt 30 ]];
 do
+    echo " Trying to connect to $AIRFLOW_DATABASE_HOST $AIRFLOW_DATABASE_USERNAME $AIRFLOW_DATABASE_NAME $AIRFLOW_DATABASE_PORT_NUMBER"
     if PGPASSWORD=$AIRFLOW_DATABASE_PASSWORD psql -h "$AIRFLOW_DATABASE_HOST" -U "$AIRFLOW_DATABASE_USERNAME" -d "$AIRFLOW_DATABASE_NAME" -c "" -p "$AIRFLOW_DATABASE_PORT_NUMBER" > /dev/null ; then
         echo "Database is ready";
         res=0
